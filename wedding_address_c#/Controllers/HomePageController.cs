9d210e2021bb683dc502a9f3143f93682a5dca6c
tienDat8386 <datnguyen83.fpt@gmail.com>
2025-06-01 15:44:22 +0700

    create project

---
+++ wedding_address_c#/Controllers/HomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wedding_address_c_.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
