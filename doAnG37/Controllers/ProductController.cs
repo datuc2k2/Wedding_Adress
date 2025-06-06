e2dda0678e72c3bd23c4ebcdb9c6671d981e1ae5
tienDat8386 <datnguyen83.fpt@gmail.com>
2025-06-06 17:09:13 +0700

    fix

---
+++ doAnG37/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace doAnG37.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
--- wedding_address_c#/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace wedding_address_c_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
